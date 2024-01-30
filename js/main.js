cd826baeb710786c9081757cb23a28f50cc6ae06
Darr Or <darrmorgan@gmail.com>
2024-01-30 11:10:31 -0500

    Randomly change ball color on click

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ var gBallDiameter = 100
 function onBallClick(elBall){
     var incrementSize = getRandomInt(20, 61)
     gBallDiameter += incrementSize
-    
     if(gBallDiameter > 400) gBallDiameter = 100
-   
     elBall.style.height = `${gBallDiameter}px`
     elBall.style.width = `${gBallDiameter}px`
-
     elBall.innerHTML=`${gBallDiameter}`
+    
+    var ballColor = getRandomColor()
+    elBall.style.backgroundColor = `${ballColor}`
+
 
 }
