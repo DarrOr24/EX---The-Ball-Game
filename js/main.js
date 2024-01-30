a998f702d0918beee13ad64391a7dbe179266aff
Darr Or <darrmorgan@gmail.com>
2024-01-30 11:22:47 -0500

    Add different arguments of max diameter per each ball

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 var gBallDiameter = 100
 
-function onBallClick(elBall){
+function onBallClick(elBall, maxDiameter){
     var incrementSize = getRandomInt(20, 61)
     gBallDiameter += incrementSize
-    if(gBallDiameter > 400) gBallDiameter = 100
+    if(gBallDiameter > maxDiameter) gBallDiameter = 100
     elBall.style.height = `${gBallDiameter}px`
     elBall.style.width = `${gBallDiameter}px`
     elBall.innerHTML=`${gBallDiameter}`
