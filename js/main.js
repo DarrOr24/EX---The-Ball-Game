8c048db17c09b39d1ad07ec48814e54cdb50e604
Darr Or <darrmorgan@gmail.com>
2024-01-30 09:50:07 -0500

    Limit diameter size

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var gBallDiameter = 100
 
 function onBallClick(elBall){
     gBallDiameter *= 1.5
+
+    if(gBallDiameter > 400) gBallDiameter = 100
    
     elBall.style.height = `${gBallDiameter}px`
     elBall.style.width = `${gBallDiameter}px`
